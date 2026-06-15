03f7bb5fb7203e47047b9ae9d3656e9375e4e866
Robert Pincus <Robert.Pincus@columbia.edu>
2026-06-14 22:35:03 -0400

    Update recipe for 1.9.3, update scripts (#14)
    * Update recipe for 1.9.3, update scripts
    * Native builds for osx arm64
    * MNT: Re-rendered with conda-smithy 2026.6.14 and conda-forge-pinning 2026.06.14.21.57.4
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,10 +7,11 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
 BUILD_DIR=build
 BUILD_TYPE=RelWithDebInfo
-
-BUILD_TESTING=OFF
 BUILD_SHARED_LIBS=ON
-RTE_ENABLE_SP=OFF
+
+BUILD_C_HEADERS=ON
+BUILD_TESTING=ON
+ENABLE_SP=OFF
 KERNEL_MODE=default
 FAILURE_THRESHOLD='7.e-4'
 
@@ -21,20 +22,26 @@ mkdir -p $PREFIX/include
 
 # Note: $CMAKE_ARGS is automatically provided by conda-forge. 
 # It sets default paths and platform-independent CMake arguments.
+# Note duplicates for shared libs - updated in later revisions of rte CMakelists
 cmake -S . -B ${BUILD_DIR} \
       ${CMAKE_ARGS} \
       -DCMAKE_Fortran_COMPILER=$FC \
-      -DRTE_ENABLE_SP=$RTE_ENABLE_SP \
-      -DKERNEL_MODE=$KERNEL_MODE \
-      -DBUILD_TESTING=$BUILD_TESTING \
-      -DFAILURE_THRESHOLD=$FAILURE_THRESHOLD \
-      -DBUILD_SHARED_LIBS=$BUILD_SHARED_LIBS \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+      -DBUILD_SHARED_LIBS=$BUILD_SHARED_LIBS \
+      -DRTE_SHARED_LIBS=$BUILD_SHARED_LIBS \
+      -DRTE_BUILD_C_HEADERS=$BUILD_C_HEADERS \
+      -DRTE_BUILD_TESTING=$BUILD_TESTING \
+      -DRTE_ENABLE_SP=$RTE_ENABLE_SP \
+      -DRTE_KERNEL_MODE=$KERNEL_MODE \
+      -DFAILURE_THRESHOLD=$FAILURE_THRESHOLD \
       -G Ninja
 
 # Compile
 cmake --build ${BUILD_DIR} --target install -- -v
 
-# Run tests
-ctest --output-on-failure --test-dir ${BUILD_DIR} -V
+# Download data to be installed in package
+ctest --output-on-failure --test-dir ${BUILD_DIR} -R fetch_rrtmgp_data
+
+# Run tests (should probably be part of the test section)
+ctest --output-on-failure --test-dir ${BUILD_DIR}  -V
